d857c1c01f4cec3985c6864759ef64b9b505c39d
Jun Iwatsubo <incodethx3932@gmail.com>
2014-12-27 17:27:13 +0900

    2014/12/27

--- js/background.js
+++ js/background.js
@@ -4,32 +4,54 @@ $(function(){
     // 画像のURLを格納する配列
     var urlBuffer = [];
 
+    function convertAbsUrl( src ){
+        return $("<a>").attr("href", src).get(0).href;
+    }
     // メッセージ受け取り時のイベント定義
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             //
             if (request.msg == "prepare" && request.url != "") {
+
                 // 変数
                 var html = getHtmlString(request.url);
-                var host = request.url.split('/')[2];
 
                 // 画像URLを格納する
                 var imageSrc = [];
                 $(html).find("img").each(function(){
-                    var re = new RegExp("^//.*?/", "i");
-                    var src = $(this).attr("src");
 
-                    var url = src.replace(re, "/");
-                    if (url.charAt(0) == "/") {
-                        url = "http://" + host + url;
+                    var url;
+                    var src = $(this).attr("src");
+                    var host = request.url.split('/')[2];
+
+                    // 
+                    var reg_base = new RegExp("^(https?://.+)/");
+                    var reg1 = new RegExp("^//(.+)$");
+                    var reg2 = new RegExp("^\.\.");
+
+                    //
+                    var base = "";
+                    if (request.url.match(reg_base)) {
+                        base = RegExp.$1;
+                    }
+                 
+                    // 
+                    if (src.match(reg_base)) {   
+                        url = src;
+                    } else if (src.charAt(0) === "/") {
+                        url = "http://" + host + src;
+                    } else if (src.match(reg1)) {
+                        url = "http://" + RegExp.$1;
+                    } else if (src.match(reg2)) {
+                        url = base + "/" + src;
                     }
                     imageSrc.push(url);
+
                 });
 
                 // 重複するURLを取り除いた配列を作成する
                 var imageUrls = imageSrc.filter(function (x, i, self) {
-                    return self.indexOf(x) === i;
-                });
+                    return self.indexOf(x) === i; });
 
                 // popup側への応答
                 sendResponse({
